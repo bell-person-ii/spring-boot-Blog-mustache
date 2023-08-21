389c5a7d7e506a5bb2dbc4f369d1d87667505383
bell-person-ii <leeji88460@gmail.com>
2023-08-21 19:16:02 +0900

    sql문 수정

--- blog-project/src/main/java/com/example/blogproject/entity/Article.java
+++ blog-project/src/main/java/com/example/blogproject/entity/Article.java
@@ -5,22 +5,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor // 디폴트 생성자 추가
 @ToString
 @Getter
 
-@Entity //DB에서 해당 객체 인식
+@Entity //DB에서 해당 객체 인식 (해당 클래스로 DB가 테이블을 생성)
 public class Article {
 
     //기본키
     @Id
-    @GeneratedValue //자동 생성
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //자동 생성
     private Long id;
     @Column // DB에 컬럼 추가
     private String title;
