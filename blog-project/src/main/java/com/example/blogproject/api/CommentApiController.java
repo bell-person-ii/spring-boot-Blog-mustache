bb54786b157db2b0d5ef6770b9f54f8348a565a1
bell-person-ii <leeji88460@gmail.com>
2023-09-08 11:12:52 +0900

    댓글 수정 기능 2 패킷 오류 개선

--- blog-project/src/main/java/com/example/blogproject/api/CommentApiController.java
+++ blog-project/src/main/java/com/example/blogproject/api/CommentApiController.java
@@ -36,6 +36,8 @@ public class CommentApiController {
     //댓글 수정
     @PatchMapping("/api/comments/{id}")
     public ResponseEntity<CommentDto> update(@PathVariable Long id,@RequestBody CommentDto dto){
+        System.out.println("Test~~~~~~~~~~~~~~~~~~~~");
+        System.out.println(dto);
         //서비스 위임
         CommentDto updatedDto = commentService.update(id,dto);
         //결과 응답
